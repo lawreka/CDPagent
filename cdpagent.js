fa4184ef5838914cf03fa21f149f59c17b4a8f6d
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2017-06-30 01:28:18 +0200

    basically done, want to sleep
    need to fix:
    - positioning of zoom, zip, whee animations
    - position of tubefriend
    - end screen

--- cdpagent.js
+++ cdpagent.js
@@ -91,6 +91,7 @@ tube = function(){
 downTube = function(){
   $(".friend").removeClass("bouncy");
   $(".friend").addClass("boop");
+  $("#pipe")[0].play();
   setTimeout(function(){
     screen7();
   }, 2000);
@@ -126,6 +127,7 @@ screen8 = function(){
         $(".water").removeClass("hide");
         $(".water").addClass("wet");
         $(".friend").addClass("whee");
+        $("#splash")[0].play();
         setTimeout(function(){
           screen9();
         }, 4000);
@@ -156,7 +158,96 @@ screen9 = function(){
         setTimeout(function(){
           $("#p5").addClass("zip");
         }, 3000);
+        setTimeout(function(){
+          screen10();
+        }, 5000);
       }
     });
   });
 }
+screen10 = function(){
+  $("#wire").addClass("hide");
+  $("#packet").removeClass("hide");
+  $(function(){
+    $("#text").typed({
+      strings: ["Each packet gets the port number and the IP address of where it's meant to go"],
+      fadeOut: true,
+      typeSpeed: 20,
+      callback: function(){
+        $("#packet").addClass("bigger");
+        $("#packettext").removeClass("hide");
+        setTimeout(function(){
+          screen11();
+        }, 4000);
+      }
+    });
+  });
+}
+screen11 = function(){
+  $("#packet").addClass("hide");
+  $(".halflength").removeClass("hide");
+  $(".packet2").removeClass("hide");
+  $(function(){
+    $("#text").typed({
+      strings: ["So that all the packets arrive in the right place"],
+      fadeOut: true,
+      typeSpeed: 20,
+      callback: function(){
+        $("#p6").addClass("gohalf");
+        setTimeout(function(){
+          $("#p7").addClass("gohalf");
+        }, 1000);
+        setTimeout(function(){
+          $("#p8").addClass("gohalf");
+        }, 1500);
+        setTimeout(function(){
+          $("#p9").addClass("gohalf");
+        }, 2000);
+        setTimeout(function(){
+          $("#p10").addClass("gohalf");
+        }, 3000);
+        setTimeout(function(){
+          screen12();
+        }, 5000);
+      }
+    });
+  });
+}
+screen12 = function(){
+  $("#lastfriend").removeClass("hide");
+  $(".halflength").addClass("hide");
+  $(".packet2").removeClass("hide");
+  $(function(){
+    $("#text").typed({
+      strings: ["And put me back together!"],
+      fadeOut: true,
+      typeSpeed: 20,
+      callback: function(){
+        setTimeout(function(){
+          screen13();
+        }, 5000);
+      }
+    });
+  });
+}
+screen13 = function(){
+  $(".packet2").addClass("hide");
+  $("#lastfriend").addClass("hide");
+  $("#facediv").removeClass("hide");
+  $("#fullface").addClass("purp");
+  $("#text").removeClass("txt2");
+  $("#text").addClass("txt");
+  $(function(){
+    $("#text").typed({
+      strings: ["Any questions?"],
+      fadeOut: true,
+      typeSpeed: 20,
+      callback: function(){
+        setTimeout(function(){
+          screen14();
+        }, 5000);
+      }
+    });
+  });
+}
+screen14 = function(){}
